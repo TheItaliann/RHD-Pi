9839fc2096425ea3af9adc400ef3a20a844f836a
Denis Moeller <144907470+NinjaV2Kn@users.noreply.github.com>
2024-01-24 13:49:52 +0100

    added the first version of the Decoder

---
+++ ppm-decoder.py
@@ -0,0 +1,30 @@
+from ppm_reader import PPMReader
+import time
+ppm_pin_id = 28
+ppm_channels = 3
+ppmReader=PPMReader(ppm_pin_id,ppm_channels)
+
+def connect() -> bool:
+    """Wait for the connection with the Remote"""
+    while ppmReader.get_valid_packets() == 0:
+        print("Waiting for the Remote to connect...")
+        time.sleep(0.5)
+    print("Remote connected!")
+    return True
+
+def connection() -> bool:
+    """Check if the Remote is still connected"""
+    last_paccket_time=ppmReader.time_since_last_packet()
+    print("Last packet time: "+str(last_paccket_time))
+    if last_paccket_time > 25000: # if 25ms without packets
+        print("Remote disconnected!")
+        return False
+    else:
+        # if a packet is received within 25ms
+        print("connected")
+        return True
+
+def reader() -> None:
+    # read the PPM signal and check connection
+    
+    print(ppmReader.get_values())
