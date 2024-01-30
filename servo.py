6a7e8b43883a88f58aa1249b58c2f5456291fd14
Emanuele <144907629+TheItaliann@users.noreply.github.com>
2024-01-30 13:31:49 +0100

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,39 +1,115 @@
-# Bibliotheken laden
-from machine import Pin, PWM
 from time import sleep
+from machine import Pin,PWM
 
-# GPIO für Steuersignal
 servo_pin = 28
 
-# 0 Grad
 grad000 = 1638
-# 90 Grad
-grad090 = 4915
-# 180 Grad
 grad180 = 8192
+
+
+
+step_size = (grad180 - grad000) // 18 
+
+
+grad010 = grad000 + step_size * 1
+grad020 = grad000 + step_size * 2
+grad030 = grad000 + step_size * 3
+grad040 = grad000 + step_size * 4
+grad050 = grad000 + step_size * 5
+grad060 = grad000 + step_size * 6
+grad070 = grad000 + step_size * 7
+grad080 = grad000 + step_size * 8
+grad090 = grad000 + step_size * 9
+grad100 = grad000 + step_size * 10
+grad110 = grad000 + step_size * 11
+grad120 = grad000 + step_size * 12
+grad130 = grad000 + step_size * 13
+grad140 = grad000 + step_size * 14
+grad150 = grad000 + step_size * 15
+grad160 = grad000 + step_size * 16
+grad170 = grad000 + step_size * 17
+grad180 = grad000 + step_size * 18
 
 pwm = PWM(Pin(servo_pin))
 pwm.freq(50)
 
-print('Position: Mitte (90 Grad)')
-pwm.duty_u16(grad090)
-sleep(2)
+while True:
+    print("10")
+    pwm.duty_u16(grad010)
+    sleep(0.1)
 
-print('Position: Ganz Links (0 Grad)')
-pwm.duty_u16(grad000)
-sleep(2)
 
-print('Position: Mitte (90 Grad)')
-pwm.duty_u16(grad090)
-sleep(2)
+    print("20")
+    pwm.duty_u16(grad010)
+    sleep(0.1)
 
-print('Position: Ganz Rechts (180 Grad)')
-pwm.duty_u16(grad180)
-sleep(2)
+    print("30")
+    pwm.duty_u16(grad030)
+    sleep(0.2)
 
-print('Position: Mitte (90 Grad)')
-pwm.duty_u16(grad090)
-sleep(2)
+    print("40")
+    pwm.duty_u16(grad040)
+    sleep(0.0001)
 
-pwm.deinit()
-print('Ende')
+    print("50")
+    pwm.duty_u16(grad050)
+    sleep(0.2)
+
+    print("60")
+    pwm.duty_u16(grad060)
+    sleep(0.2)
+
+    print("70")
+    pwm.duty_u16(grad070)
+    sleep(0.00000000000001)
+
+    print("80")
+    pwm.duty_u16(grad080)
+    sleep(0.2)
+
+    print("90")
+    pwm.duty_u16(grad090)
+    sleep(0.1)
+
+    print("100")
+    pwm.duty_u16(grad100)
+    sleep(0.2)
+
+    print("110")
+    pwm.duty_u16(grad110)
+    sleep(0.2)
+
+    print("120")
+    pwm.duty_u16(grad120)
+    sleep(0.2)
+
+    print("130")
+    pwm.duty_u16(grad130)
+    sleep(0.5)
+
+    print("140")
+    pwm.duty_u16(grad140)
+    sleep(0.05)
+
+    print("150")
+    pwm.duty_u16(grad150)
+    sleep(0.5)
+
+    print("160")
+    pwm.duty_u16(grad160)
+    sleep(0.3)
+
+    print("170")
+    pwm.duty_u16(grad170)
+    sleep(0.005)
+
+    print("180!!!!!")
+    pwm.duty_u16(grad180)
+    sleep(0.1)
+
+    print("0")
+    pwm.duty_u16(grad000)
+    sleep(0.01)
+    pwm.deinit()
+    print('Ende')
+
