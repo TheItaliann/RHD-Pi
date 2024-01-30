68874b634c7e8d115b678cb046bc9365ba730223
Emanuele <144907629+TheItaliann@users.noreply.github.com>
2024-01-30 11:04:29 +0100

    Update servo.py

--- servo.py
+++ servo.py
@@ -6,37 +6,33 @@ from time import sleep
 servo_pin = 28
 
 # 0 Grad
-grad000 = 500000
-# 45 Grad
-grad045 = 1000000
+grad000 = 1638
 # 90 Grad
-grad090 = 1500000
-# 135 Grad
-grad135 = 2000000
+grad090 = 4915
 # 180 Grad
-grad180 = 2500000
+grad180 = 8192
 
 pwm = PWM(Pin(servo_pin))
 pwm.freq(50)
 
 print('Position: Mitte (90 Grad)')
-pwm.duty_ns(grad090)
+pwm.duty_u16(grad090)
 sleep(2)
 
 print('Position: Ganz Links (0 Grad)')
-pwm.duty_ns(grad000)
+pwm.duty_u16(grad000)
 sleep(2)
 
 print('Position: Mitte (90 Grad)')
-pwm.duty_ns(grad090)
+pwm.duty_u16(grad090)
 sleep(2)
 
 print('Position: Ganz Rechts (180 Grad)')
-pwm.duty_ns(grad180)
+pwm.duty_u16(grad180)
 sleep(2)
 
 print('Position: Mitte (90 Grad)')
-pwm.duty_ns(grad090)
+pwm.duty_u16(grad090)
 sleep(2)
 
 pwm.deinit()
