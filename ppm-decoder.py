b0b0d776c75fa5b9389156ade82eeabf677df7b9
Emanuele <144907629+TheItaliann@users.noreply.github.com>
2024-01-31 12:11:03 +0100

    Update ppm-decoder.py
    added some test things

--- ppm-decoder.py
+++ ppm-decoder.py
@@ -11,7 +11,7 @@ class PPMDecoder:
         self.button = Pin(self.pin3, Pin.IN, Pin.PULL_UP)
         self.channels = [0, 0, 0]
 
-    def callback(self):
+    def callback(self) -> list:
         self.time_servo = Pulse(self.pin1, 1, 1_000_000)       
         self.time_throttle = Pulse(self.pin2, 1, 1_000_000)       
         self.time_button = Pulse(self.pin3, 1, 1_000_000)
@@ -20,20 +20,23 @@ class PPMDecoder:
         self.channels[2] = self.time_button      
         return self.channels
     
-    def button(self):
-        if self.time_button > 900:
-            return True
-        else:
-            return False
     
 if __name__ == "__main__":
-    ppm = PPMDecoder(27, 26, 22)
+    ppm = PPMDecoder(27, 21, 22)
     while True:
         values = ppm.callback()
         state = ppm.button()
         servo = values[0]
         throttle = values[1]
         button = values[2]
-        print("Servo: ", servo, "Throttle: ", throttle, "Button: ", button, "State: ", state)
+        print("Servo: ", servo, "Throttle: ", throttle, "Button: ", button,)
         print("")
         time.sleep(1)
+        if button > 900:
+            button_state = "Off"
+        else:
+            button_state = "On"
+        print(f"button is {button_state}")
+        
+        steering = ((servo - 1198) / (1785 - 1200)) * 100
+        print(f"{int(steering)}%")
