b3aaff59a176f3731bd19d21cec2a9d2cd70a696
Emanuele <144907629+TheItaliann@users.noreply.github.com>
2024-01-31 10:26:52 +0100

    Update funkServo.py

--- funkServo.py
+++ funkServo.py
@@ -1,11 +1,14 @@
 from time import sleep
 from machine import Pin,PWM
+from PPMDecoder import callback
 
 servo_pin = 28
 pwm = PWM(Pin(servo_pin))
 pwm.freq(50)
 
 def servo_str():
-    Wert = int(input("eingabe PWM: "))
-    pwm.duty_u16(Wert)
-servo_str()
+    pwm.duty_u16(valServo)
+
+while True:
+    servo_str()
+    sleep(1)
