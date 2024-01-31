bb7fd41ee7c477e94b7a950c1c63fab0d07cffd1
Denis Moeller <144907470+NinjaV2Kn@users.noreply.github.com>
2024-01-31 10:37:14 +0100

    wrote a new code for the decoder

--- ppm-decoder.py
+++ ppm-decoder.py
@@ -1,29 +1,21 @@
-from machine import Pin
+from machine import Pin, time_pulse_us as time_pulse_us
 import time
-ppm_pin_id = 27
-ppm_channels = 3
 
-ppm_pin = Pin(ppm_pin_id, Pin.IN, Pin.PULL_UP)
-ppm_channels_values = [0] * ppm_channels
-timer = time.ticks_us()
+class PPMDecoder:
+    def __init__(self, pins, channel) -> None:
+        self.pins = []
+        self.channels = []
+        for i in range(pins):
+            self.pins.append(Pin(i, Pin.IN, Pin.PULL_UP))
+        for j in range(channel):
+            self.channels = [0] * self.channels
 
-
-def ppm_callback(pin):
-    now = time.ticks_us()
-    delta = time.ticks_diff(now,timer)
-    timer = now
-    
-    global ppm_channels_values
-    ppm_channels_values = [0] * ppm_channels
-    for i in range(1, ppm_channels):
-        while ppm_pin.value() == 1:
-            pass
-        ppm_channels_values[i] = delta
-
-ppm_pin.irq(trigger=Pin.IRQ_RISING, handler=ppm_callback)
-
-while True:
-    print(ppm_channels_values)
-    time.sleep(1)
-
-
+    def callback(self):
+        current_channel = 0
+        if len(self.channels) == len(self.pins):
+            for Pin in self.pins:
+                istime = Pulse(Pin, 0, 1_000_000)
+                if istime > 0:
+                    self.channels[current_channel] = istime
+                    current_channel += 1
+        return self.channels
