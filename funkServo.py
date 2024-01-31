b08e85d09045d49a7fb118f0b13c6f81750da5f2
Emanuele <144907629+TheItaliann@users.noreply.github.com>
2024-01-31 13:51:46 +0100

    Update funkServo.py

--- funkServo.py
+++ funkServo.py
@@ -2,9 +2,15 @@ from time import sleep
 from machine import Pin,PWM
 from PPMDecoder import callback
 
+def map_value(percentage:int) -> int:
+    return int((percentage - 0) * (8620 - 1638) / (100 - 0) + 1638)
+
 servo_pin = 28
 pwm = PWM(Pin(servo_pin))
 pwm.freq(50)
+
+values = callback()
+valServo = map_value(100)
 
 def servo_str():
     pwm.duty_u16(valServo)
