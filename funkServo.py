20d5565516a2d40a0cab61c5766f507a71d87ab7
Emanuele <144907629+TheItaliann@users.noreply.github.com>
2024-01-31 08:09:28 +0100

    Update funkServo.py

--- funkServo.py
+++ funkServo.py
@@ -2,9 +2,10 @@ from time import sleep
 from machine import Pin,PWM
 
 servo_pin = 28
-
+pwm = PWM(Pin(servo_pin))
+pwm.freq(50)
 
 def servo_str():
-    pwm = PWM(Pin(servo_pin))
     Wert = int(input("eingabe PWM: "))
     pwm.duty_u16(Wert)
+servo_str()
