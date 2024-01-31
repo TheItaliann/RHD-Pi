da36c7b2b57be9fd72c3aa235b5e03690601ce44
Emanuele <144907629+TheItaliann@users.noreply.github.com>
2024-01-31 08:53:50 +0100

    Update ppm-decoder.py

--- ppm-decoder.py
+++ ppm-decoder.py
@@ -1,22 +1,29 @@
 from machine import Pin
 import time
-ppm_pin_id = 28
+ppm_pin_id = 27
 ppm_channels = 3
 
 ppm_pin = Pin(ppm_pin_id, Pin.IN, Pin.PULL_UP)
 ppm_channels_values = [0] * ppm_channels
+timer = time.ticks_us()
+
 
 def ppm_callback(pin):
+    now = time.ticks_us()
+    delta = time.ticks_diff(now,timer)
+    timer = now
+    
     global ppm_channels_values
     ppm_channels_values = [0] * ppm_channels
-    ppm_channels_values[0] = time.ticks_us()
     for i in range(1, ppm_channels):
         while ppm_pin.value() == 1:
             pass
-        ppm_channels_values[i] = time.ticks_us()
+        ppm_channels_values[i] = delta
 
 ppm_pin.irq(trigger=Pin.IRQ_RISING, handler=ppm_callback)
 
 while True:
     print(ppm_channels_values)
     time.sleep(1)
+
+
