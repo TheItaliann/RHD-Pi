9941b5f9cf94de96773893d0ef8204f23ea28d46
Emanuele <144907629+TheItaliann@users.noreply.github.com>
2024-02-01 13:05:35 +0100

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,27 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 
-motor_pin = 4  # Beispiel-GPIO-Pin (ersetzen Sie dies durch Ihren gewünschten Pin)
+motor_pin = 4 # Beispiel-GPIO-Pin (ersetzen Sie dies durch Ihren gewünschten Pin)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(motor_pin, GPIO.OUT)
 
-pwm_frequency = 1000
-pwm = GPIO.PWM(motor_pin, pwm_frequency)
-pwm.start(0)
-
 try:
     while True:
-        eingabe = input("Eingabe (0-100, 'q' zum Beenden): ")
+        eingabe = input("Drücken Sie Enter, um den Motor zu starten (q zum Beenden): ")
         if eingabe.lower() == 'q':
             break  # Beenden Sie die Schleife, wenn 'q' eingegeben wird
-        duty_cycle = float(eingabe)
-        pwm.ChangeDutyCycle(duty_cycle)
-        time.sleep(0.1)  # Verringern Sie den Sleep-Wert für schnellere Reaktion
+
+        # Motor starten
+        GPIO.output(motor_pin, GPIO.HIGH)
+        time.sleep(3)  # Motor für 3 Sekunden laufen lassen
+
+        # Motor stoppen
+        GPIO.output(motor_pin, GPIO.LOW)
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    pwm.stop()
     GPIO.cleanup()
