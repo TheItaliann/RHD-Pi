951419aca758c3b45e126db7b6445c0ebd810989
Emanuele <144907629+TheItaliann@users.noreply.github.com>
2024-03-14 10:55:07 +0100

    Update fehler.py

--- fehler.py
+++ fehler.py
@@ -1,46 +1,17 @@
+# Bibliotheken laden
 from machine import Pin
-from utime import ticks_ms, ticks_diff, sleep
 
+# Initialisierung von GPIO16 als Eingang
+sensor_d = Pin(21, Pin.IN)
 
-ir_pin = Pin(28, Pin.IN)
+# Zähler
+count = 0
 
+# Funktion: Interrupt-Behandlung
+def sensor_irq(pin):
+    global count
+    count += 1
+    print(count)
 
-distance_traveled = 0
-turns = 0  
-count = 0 
-
-hole_dis_cm = 0.22  
-wheel_u_cm = 2.5 * 3.1416  
-
-
-last_time = ticks_ms()
-
-def detect_object():
-    global distance_traveled, turns, count
-    if ir_pin.value() == 1:
-        distance_traveled += hole_dis_cm
-        count += 1
-        if count == 20:
-            turns += 1
-            count = 0
-
-def measure_speed():
-    global last_time
-    if ir_pin.value() == 1:
-        now_time = ticks_ms()
-        time_ms = ticks_diff(now_time, last_time)
-        last_time = now_time
-        holes = time_ms / (1000 * hole_dis_cm)
-        # Berechne die zurückgelegte Strecke
-        distance_cm = holes * hole_dis_cm
-        # Berechne die Geschwindigkeit in km/h
-        speed_kmh = (distance_cm / 100000) / (time_ms / 3600000)
-        return speed_kmh
-    return 0
-
-while True:
-    detect_object()
-    print(f"Zurückgelegte Strecke: {distance_traveled:.2f} cm | Umdrehungen: {turns}")
-    velocity = measure_speed()
-    print(f"Geschwindigkeit: {velocity:.2f} km/h")
-    sleep(1)
+# Interrupt-Steuerung
+sensor_d.irq(trigger=Pin.IRQ_RISING, handler=sensor_irq)
